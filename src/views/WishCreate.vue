cb6f5d4687694fee05e3730e9e1a5db77bbc792f
BelleHan <heesu1028@naver.com>
2023-07-18 22:01:51 +0900

    create wish

--- src/views/WishCreate.vue
+++ src/views/WishCreate.vue
@@ -1,18 +1,53 @@
 <template>
-    <div>Wish Create</div>
-    <!-- 음악가 곡명 장르 완주목표기간 완주여부 -->
-    <form action="">
-        <input type="text" />
-        <input type="text" />
-        <input type="text" />
-        <input type="text" />
-        <input type="text" />
-        <button type="button">등록</button>
-    </form>
+  <div>Wish Create</div>
+  <form @submit="createWish">
+    <label>음악가</label>
+    <input type="text" v-model="wish.musician" /><br />
+    <label>곡명</label>
+    <input type="text" v-model="wish.music" /><br />
+    <label>장르</label>
+    <input type="text" v-model="wish.genre" /><br />
+    <label>완주목표기간</label>
+    <input type="text" v-model="wish.dueDate" /><br />
+    <label>녹음여부</label>
+    <input type="text" v-model="wish.isRecord" /><br />
+    <button type="submit">등록</button>
+  </form>
 </template>
 
-<script setup>
+<script>
+import axios from 'axios'
+import { ref } from 'vue'
 
+export default {
+  setup() {
+    const wish = ref({
+      musician: '',
+      music: '',
+      genre: '',
+      dueDate: '',
+      isRecord: ''
+    })
+
+    const createWish = async () => {
+      let res
+      const data = {
+        musician: wish.value.musician,
+        music: wish.value.music,
+        genre: wish.value.genre,
+        dueDate: wish.value.dueDate,
+        isRecord: wish.value.isRecord
+      }
+
+      res = await axios.post('http://localhost:3000/wishlist', data)
+    }
+
+    return {
+      wish,
+      createWish
+    }
+  }
+}
 </script>
 
 <style lang="scss" scoped></style>
